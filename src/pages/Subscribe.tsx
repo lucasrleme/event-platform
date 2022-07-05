64f4e7e28af54afcaa5d3ad4b73c831a470407a8
Lucas Leme <lucasratm40@gmail.com>
2022-07-05 18:06:56 -0300

    Responsive tailwind in subscribe page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -29,10 +29,14 @@ export function Subscribe(){
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
+      <div className="flex flex-col items-center justify-between px-8 mt-20 
+        md:w-full md:max-w-[1100px] md:mx-auto md:flex-row"
+      >
+        <div className="mb-8 md:max-w-[640px]">
           <Logo />
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+          <h1 className="text-[2rem] leading-tight 
+            mt-8 md:text-[2.5rem]"
+          >
             Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong> 
           </h1>
           <p className="mt-4 text-gray-200 leading-relaxed">
